c482286f4b3f1b58e40a923e0db279311eaa4d35
larry <xspanni@gmail.com>
2021-12-28 18:28:40 +0800

    去掉Qt Quick相关内容，减少编译WebAssembly文件的大小

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
 #include "src/md5.h"
 #include "src/wasm/wasm.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
-
-  QQmlApplicationEngine engine;
-  const QUrl url(QStringLiteral("qrc:/src/qrc/textfield.qml"));
-
-  engine.load(url);
-
-  if (engine.rootObjects().isEmpty())
-    return -1;
-
-  //std::cout << "计算MD5：" << calcSha512("啊哈哈abc222") << endl;
-
-  return app.exec();
+  std::cout << "计算MD5：" << calcSha512("啊哈哈abc222") << endl;
 }
 
