990a32560996760d052374d259dab33785f91b00
larry <xspanni@gmail.com>
2022-02-21 22:47:05 +0800

    尝试通过qt处理json

--- src/stele/stele.cpp
+++ src/stele/stele.cpp
@@ -7,7 +7,14 @@
 #include <QJsonObject>
 
 std::string buildHtml(const std::string &content) {
-    QJsonParseError jsonError;
-    auto jsonDoc = QJsonDocument::fromJson(content.c_str(), &jsonError);
-    
+  QJsonParseError jsonError;
+  auto jsonDoc = QJsonDocument::fromJson(content.c_str(), &jsonError);
+  qDebug() << "json转换结果：" << jsonError.errorString();
+  QJsonObject jsonObject = jsonDoc.object();
+
+      foreach(const QString &key, jsonObject.keys()) {
+      QJsonValue value = jsonObject.value(key);
+      qDebug() << "Key = " << key << ", Value = " << value.toString();
+    }
+
 }
